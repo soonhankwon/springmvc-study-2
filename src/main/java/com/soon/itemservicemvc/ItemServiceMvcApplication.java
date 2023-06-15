d0107bcaee4072a9cd139ca44af81f53bacc17b3
soonable@naver.com <soonable@naver.com>
2023-06-15 11:54:59 +0900

    Initial commit

---
+++ src/main/java/com/soon/itemservicemvc/ItemServiceMvcApplication.java
@@ -0,0 +1,13 @@
+package com.soon.itemservicemvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemServiceMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soon/itemservicemvc/ItemServiceMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soon.itemservicemvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
