8716327246c935f42d6df4bc1a821239058dc771
soonable@naver.com <soonable@naver.com>
2023-06-15 16:55:59 +0900

    feat : 타임리프

---
+++ src/main/java/com/soon/itemservicemvc/web/basic/BasicItemController.java
@@ -0,0 +1,36 @@
+package com.soon.itemservicemvc.web.basic;
+
+import com.soon.itemservicemvc.domain.item.Item;
+import com.soon.itemservicemvc.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * test data
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+}
